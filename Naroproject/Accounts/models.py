7f0b117dfc91f75fd0d6c72695ab3350718aa5b6
Woo-Yeol <king199276@gmail.com>
2020-09-16 21:08:58 +0900

    Create SignUp Func

--- Naroproject/Accounts/models.py
+++ Naroproject/Accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class myUser(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    The_date_of_ones_birth = models.DateTimeField(default='1950-01-01')
+    email = models.CharField(max_length=40, default='example@example.com')
